3af3158336ac2622d21e6099b3f5de6678fba6d3
Catiel <oliver.alandiaa@gmail.com>
2024-04-01 00:06:11 -0400

    parte visual sin funcionalidad

--- eduApp/settings.py
+++ eduApp/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'EducaPlus/static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_DIRS = [BASE_DIR / 'EducaPlus' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
