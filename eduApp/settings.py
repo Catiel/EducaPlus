16149c1b38915f078468226637b48ab845daf6a9
Catiel <oliver.alandiaa@gmail.com>
2024-04-24 00:03:51 -0400

    configuracion email

--- eduApp/settings.py
+++ eduApp/settings.py
@@ -134,7 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'tu_correo@example.com'  # Tu dirección de correo electrónico para autenticación SMTP
-EMAIL_HOST_PASSWORD = 'tu_contraseña'  # Tu contraseña de correo electrónico para autenticación SMTP
+EMAIL_HOST_USER = 'eduplus720@gmail.com'
+EMAIL_HOST_PASSWORD = 'jjbm pqfp nsfy ieks'
 
 
