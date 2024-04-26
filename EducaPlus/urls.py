eb0d7bcc060b5046d2538dfcd5b231901703e1d5
Diego3520 <202010017@est.umss.edu>
2024-04-26 19:27:50 -0400

    Bugsfaltantes por Diego Espinoza Lucero

--- EducaPlus/urls.py
+++ EducaPlus/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('add_cart/<int:curso_id>/', views.add_cart, name='add_cart'),
     path('obtener_contador_carrito/', views.obtener_contador_carrito, name='obtener_contador_carrito'),
     path('login/', views.login_view, name='login'),
+    path('api/verificar-correo-teach/', views.verificar_correo_teach, name='verificar_correo_teach'),
 ]
