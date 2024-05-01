867ed978d33ba011d3fa9fccf388910a4380f9a9
Paola Galindo <202103796@est.umss.edu>
2024-05-01 13:37:06 -0500

    redireccion a index al cambiar la contraseña

--- EducaPlus/views.py
+++ EducaPlus/views.py
@@ -343,7 +343,7 @@ def change_password(request, uidb64):
         user.password = make_password(new_password)
         user.save()
 
-        return HttpResponse('Contraseña actualizada con éxito')
+        return redirect ('index')
 
     return HttpResponse('Método no permitido', status=405)
 
